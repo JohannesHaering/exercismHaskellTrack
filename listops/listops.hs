e14711e404114e45424e71303207c43e5e97bab5
Johannes Haering <johannes.haering99@gmx.de>
2021-03-10 16:00:55 +0100

    Added remaining list ops

--- listops/listops.hs
+++ listops/listops.hs
@@ -18,3 +18,19 @@ lentghHelper [] = 0
 lengthHelper (x:xs) 
     | xs == [] = 1
     | otherwise = 1 + (lengthHelper xs)
+
+
+foldler fun acc [] = acc
+foldler fun acc (x:xs)
+    | xs == [] = fun acc x
+    | otherwise = foldrer fun (fun acc x) xs
+
+foldrer fun acc [] = acc
+foldrer fun acc (x:xs)
+    | xs == [] = fun acc x
+    | otherwise = fun x (foldrer fun acc xs)
+
+rev xs = revHelper xs []
+    where
+      revHelper [] acc = acc
+      revHelper (x:xs) acc = revHelper xs (x:acc)   
