c6f5a2b9c4b6cbfbfac1c1f09f067774ba16041a
Johannes Haering <johannes.haering99@gmx.de>
2021-03-10 21:24:22 +0100

    Implemented version with lambdas

--- largestseriesproduct/largestseriesproduct.hs
+++ largestseriesproduct/largestseriesproduct.hs
@@ -1,16 +1,21 @@
 getDigits 0 = []
 getDigits num = mod num 10 : getDigits (div num 10)
 
-largestProduct series n = helper digits (getArrayOfSize (n-1)) 0
+largest series n fun = helper digits (getArrayOfSize (n-1)) 0
   where
---    helper :: Num a => [a] -> [a] -> a -> a
     helper (x:xs) accs maxProd
-      | xs == [] = max maxProd ((last accs) * x)
-      | otherwise = helper xs (updateAccs accs x 1) (max maxProd ((last accs) * x))
+      | xs == [] = max maxProd (fun (last accs) x)
+      | otherwise = helper xs (updateAccs accs x 1 fun) (max maxProd (fun (last accs) x))
     digits = reverse (getDigits series)
-    updateAccs (acc:accs) x temp
-      | accs == [] = [temp * x]
-      | otherwise = (temp * x) : (updateAccs accs x acc)
+    updateAccs (acc:accs) x temp fun
+      | accs == [] = [fun temp x]
+      | otherwise = (fun temp x) : (updateAccs accs x acc fun)
+
+largestProduct series n = largest series n (*)
+
+largestSum series n = largest series n (+)
+
+largestDifference series n = largest series n (-)
 
 getArrayOfSize n
   | n <= 0 = []
